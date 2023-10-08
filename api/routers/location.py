3d337f1dffae6172dce78fd0d441c2c540ea2b1c
blackbear <zoeytu0919@gmail.com>
2023-10-07 21:36:33 -0400

    lol

--- api/routers/location.py
+++ api/routers/location.py
@@ -2,6 +2,7 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from sqlalchemy import desc
 
 from api import dependencies, models,database,schemas
 
@@ -24,3 +25,16 @@ async def get_all_locations(
         .limit(commons["limit"])
         .all()
     )
+
+@router.get("/{location_id}/feed", response_model=list[schemas.Location])
+async def get_feed_from_location(
+    location_id: int,
+    commons: Annotated[dict, Depends(dependencies.common_parameters)],
+    db: Session = Depends(database.get_db)
+):
+    return (db.query(models.Post).filter_by(location_id=location_id)
+            .order_by(desc(models.Post.datetime))
+            .offset(commons["skip"])
+        .limit(commons["limit"])
+        .all())
+
